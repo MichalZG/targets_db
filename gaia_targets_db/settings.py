2ae2ef51c1e35703132b8dbe906f448c67316b5e
michalzg <m.zejmo@codelab.pl>
2020-07-29 17:41:51 +0200

    sitesettings

--- gaia_targets_db/settings.py
+++ gaia_targets_db/settings.py
@@ -19,14 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=1zjrd_2s3#ly+#)p(_f8t)8-w)kx3%(#6vc%ry%=7k-%a$x-&'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
