d8cccaabaca90a5fa9ae777023cd7d9d7acee640
michalzg <m.zejmo@codelab.pl>
2020-07-30 13:35:28 +0200

    admin list

--- targets/admin.py
+++ targets/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Target
 
-admin.site.register(Target)
+
+@admin.register(Target)
+class TargetAdmin(admin.ModelAdmin):
+    model = Target
+
+    list_display = ('name', 'ra', 'dec', 'magnitude', 'importance', 'priority')
+    list_display_links = ('name',)
+    list_editable = ('magnitude', 'importance', 'priority')
+    ordering = ('name',)
